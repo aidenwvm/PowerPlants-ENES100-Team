e37ab7c631899f1c18ed6819d30cddbd96fc3a8e
allyfu <afu123@terpmail.umd.edu>
2025-11-13 09:18:45 -0500

    Update servoControl.ino

--- main/servoControl.ino
+++ main/servoControl.ino
@@ -2,15 +2,16 @@
 
 //1325 = 90 degrees
 //1750 = 45 degrees
-void liftArm(float angle)
+void liftArmAngle(float angle)
 {
   float us = ((1325-1750)/(90-45))*(angle-90)+1325;
   servoArm.writeMicroseconds(us);
 }
 
-void liftArm(double height)
+void liftArmHeight(float height)
 {
   float angle = asin(height / 140);
   liftArm(angle);
 }
+
 
