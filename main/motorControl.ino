8c9d882fc5ecd12499337a2e1faef8129914e17f
Aiden Vieara McCarthy <aidenwvm@terpmail.umd.edu>
2025-11-18 08:36:50 -0500

    Update motorControl.ino
    added setMotorSpeed function, removed earlier testing code

--- main/motorControl.ino
+++ main/motorControl.ino
@@ -8,12 +8,17 @@ void motorSetup(int en, int in1, int in2)
   digitalWrite(in2, LOW);
 }
 
-void driveForward()
+void setMotorSpeed(int speed)
 {
-  digitalWrite(enLF, HIGH);
-  digitalWrite(enLR, HIGH);
-  digitalWrite(enRF, HIGH);
-  digitalWrite(enRR, HIGH);
+  analogWrite(enLF, speed);
+  analogWrite(enLR, speed);
+  analogWrite(enRF, speed);
+  analogWrite(enRR, speed);
+}
+
+void driveForward(int speed)
+{
+  setMotorSpeed(speed);
 
   digitalWrite(inLF1, HIGH);
   digitalWrite(inLF2, LOW);
@@ -23,28 +28,11 @@ void driveForward()
   digitalWrite(inRF2, LOW);
   digitalWrite(inRR1, HIGH);
   digitalWrite(inRR2, LOW);
-
-  delay(1000);
-
-  //test to find a better acceleration threshold
-  for(int i = 0; i < 90; i += 10)
-  {
-    analogWrite(enLF, i);
-    analogWrite(enLR, i);
-    analogWrite(enRF, i);
-    analogWrite(enRR, i);
-
-    delay(10);
-    currentSpeed = i;
-  }
 }
 
-void turnLeft()
+void turnLeft(int speed)
 {
-  digitalWrite(enLF, HIGH);
-  digitalWrite(enLR, HIGH);
-  digitalWrite(enRF, HIGH);
-  digitalWrite(enRR, HIGH);
+  setMotorSpeed(speed);
   
   digitalWrite(inLF1, HIGH);
   digitalWrite(inLF2, LOW);
@@ -54,37 +42,11 @@ void turnLeft()
   digitalWrite(inRF2, HIGH);
   digitalWrite(inRR1, HIGH);
   digitalWrite(inRR2, LOW);
-
-  delay(975);
-
-  /*//test to find better speed threshold for turning so shit doesn't go flying
-  for(int i = 0; i < 45; i += 5)
-  {
-    analogWrite(enLF, i);
-    analogWrite(enLR, i);
-    analogWrite(enRF, i);
-    analogWrite(enRR, i);
-
-    delay(10);
-    currentSpeed = i;
-  }*/
-
-  digitalWrite(inLF1, LOW);
-  digitalWrite(inLF2, LOW);
-  digitalWrite(inLR1, LOW);
-  digitalWrite(inLR2, LOW);
-  digitalWrite(inRF1, LOW);
-  digitalWrite(inRF2, LOW);
-  digitalWrite(inRR1, LOW);
-  digitalWrite(inRR2, LOW);
 }
 
-void turnRight()
+void turnRight(int speed)
 {
-  digitalWrite(enLF, HIGH);
-  digitalWrite(enLR, HIGH);
-  digitalWrite(enRF, HIGH);
-  digitalWrite(enRR, HIGH);
+  setMotorSpeed(speed);
   
   digitalWrite(inRF1, HIGH);
   digitalWrite(inRF2, LOW);
@@ -94,34 +56,11 @@ void turnRight()
   digitalWrite(inLF2, HIGH);
   digitalWrite(inLR1, HIGH);
   digitalWrite(inLR2, LOW);
-
-  delay(975);
-  /*
-  for(int i = 0; i < 45; i += 5)
-  {
-    analogWrite(enLF, i);
-    analogWrite(enLR, i);
-    analogWrite(enRF, i);
-    analogWrite(enRR, i);
-
-    delay(10);
-    currentSpeed = i;
-  }*/
 }
 
 void stop()
 {
-  /*for(int i = currentSpeed; i > 0; i -= 10)
-  {
-    analogWrite(enLF, i);
-    analogWrite(enLR, i);
-    analogWrite(enRF, i);
-    analogWrite(enRR, i);
-
-    delay(10);
-  }
-
-  currentSpeed = 0;*/
+  setMotorSpeed(0);
   
   digitalWrite(inLF1, LOW);
   digitalWrite(inLF2, LOW);
@@ -131,19 +70,11 @@ void stop()
   digitalWrite(inRF2, LOW);
   digitalWrite(inRR1, LOW);
   digitalWrite(inRR2, LOW);
- 
-  digitalWrite(enLF, 0);
-  digitalWrite(enLR, 0);
-  digitalWrite(enRF, 0);
-  digitalWrite(enRR, 0);
 }
 
-void backup()
+void backup(int speed)
 {
-  digitalWrite(enLF, HIGH);
-  digitalWrite(enLR, HIGH);
-  digitalWrite(enRF, HIGH);
-  digitalWrite(enLR, HIGH);
+  setMotorSpeed(speed);
 
   digitalWrite(inLF1, LOW);
   digitalWrite(inLF2, HIGH);
@@ -153,17 +84,4 @@ void backup()
   digitalWrite(inRF2, HIGH);
   digitalWrite(inRR1, LOW);
   digitalWrite(inRR2, HIGH);
-
-  delay(1000);
-
-  for(int i = 0; i < 90; i += 10)
-  {
-    analogWrite(enLF, i);
-    analogWrite(enLR, i);
-    analogWrite(enRF, i);
-    analogWrite(enRR, i);
-
-    delay(10);
-    currentSpeed = i;
-  }
 }
