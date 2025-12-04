88e9bb077cb2f161dc002646d8a29fabc0f2b0a2
ajpandya-sketch <ajpandya@terpmail.umd.edu>
2025-12-04 09:21:38 -0500

    Add files via upload

---
+++ main/testprobe.ino
@@ -0,0 +1,39 @@
+float testprobe() {
+ 
+float final_height = 80.0; //change according to actual final height(mm)
+float start_height = 100.0; //change according to actual start height(mm)
+float speed = 30.0; //change based on actual starting speed(deg/s)
+
+liftArmHeight(start_height, speed);
+speed = 10.0; //slow speed for descent of arm - can also change based on actual speed wanted
+
+String data = "";
+
+float height = start_height;
+float maxForce = 0; //keeps track of highest force
+
+//going down
+while(height > final_height){
+  height--;
+  liftArmHeight(height, speed);
+  float force = getForce();
+  data = data + String(force) + " "; 
+  if(force > maxForce){
+    maxForce = force;
+  }
+}
+
+//going back up
+while(height < start_height){
+  height++;
+  liftArmHeight(height, speed);
+  float force = getForce();
+  data = data + String(force) + " "; 
+  if(force > maxForce){
+    maxForce = force;
+  }
+}
+
+return maxForce;
+
+}
