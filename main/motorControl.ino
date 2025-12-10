3a8f0ec83f46ef2baba258ab1c04303543a419fc
allyfu <afu123@terpmail.umd.edu>
2025-12-10 12:34:21 -0500

    Update motorControl.ino

--- main/motorControl.ino
+++ main/motorControl.ino
@@ -146,6 +146,20 @@ void driveForward(float speed)
   digitalWrite(inRR2, LOW);
 }
 
+void turnLeft(float speed)
+{
+  setMotorSpeed(speed);
+  
+  digitalWrite(inRF1, HIGH);
+  digitalWrite(inRF2, LOW);
+  digitalWrite(inRR1, HIGH);
+  digitalWrite(inRR2, LOW);
+  digitalWrite(inLF1, LOW);
+  digitalWrite(inLF2, HIGH);
+  digitalWrite(inLR1, LOW);
+  digitalWrite(inLR2, HIGH);
+}
+
 void goLeft(float speed)
 {
   setMotorSpeed(speed);
@@ -187,3 +201,4 @@ void backup(float speed)
   digitalWrite(inRR1, LOW);
   digitalWrite(inRR2, HIGH);
 }
+
